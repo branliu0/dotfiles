cc8d74876e6481cbc238c488160d7a473a0c69ab
Brandon Liu <brandon.k.liu@gmail.com>
2012-01-20 21:05:07 -0500

    Update install.sh for vundle

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ git clone http://github.com/thenovices/dotfiles.git
 ln -s ~/dotfiles/.vim* .
 ln -s ~/dotfiles/.bash* .
 cd ~/dotfiles
-git submodule init
-git submodule update
+git clone http://github.com/gmarik/vundle.git .vim/bundle/vundle
+vim +BundleInstall +qall
